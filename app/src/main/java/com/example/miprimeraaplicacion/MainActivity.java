7afca8a2257135774755d8836ae5f1ce48ac17f3
mauriciomaravilla <maurimaravilla95@gmail.com>
2025-01-28 01:15:30 -0600

    creacion de la calculadora de suma

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.miprimeraaplicacion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
+    TextView tempval;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btncalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tempval = findViewById(R.id.txtnum1);
+                Double num1 = Double.parseDouble(tempval.getText().toString());
+
+                tempval = findViewById(R.id.txtnum2);
+                Double num2 = Double.parseDouble(tempval.getText().toString());
+
+                double respuesta = num1 + num2;
+
+                tempval = findViewById(R.id.lblrespuesta);
+                tempval.setText("respuesta: "+ respuesta);
+            }
+        });
     }
 }

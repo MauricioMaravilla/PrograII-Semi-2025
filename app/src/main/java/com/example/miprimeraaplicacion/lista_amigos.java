513a55f2c646f9863eb9ce74ee4b588ff480ba51
mauriciomaravilla <maurimaravilla95@gmail.com>
2025-03-27 22:35:54 -0600

    creacion de la rama amigos con couchDB y la obtencion de datos del servidor

--- app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
+++ app/src/main/java/com/example/miprimeraaplicacion/lista_amigos.java
@@ -46,6 +46,8 @@ public class lista_amigos extends Activity {
     amigos misAmigos;
     FloatingActionButton fab;
     int posicion = 0;
+    obtenerDatosServidor datosServidor;
+    detectarInternet di;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +58,7 @@ public class lista_amigos extends Activity {
 
         fab = findViewById(R.id.fabAgregarAmigo);
         fab.setOnClickListener(view -> abriVentana());
-        obtenerDatosAmigos();
+        listarDatos();
         buscarAmigos();
     }
     @Override
@@ -123,6 +125,25 @@ public class lista_amigos extends Activity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtras(parametros);
         startActivity(intent);
+
+
+    }
+    private void listarDatos(){
+        try{
+           di = new detectarInternet(this);
+           if (di.hayConexionInternet()) {//online
+               datosServidor = new obtenerDatosServidor();
+               String respuesta = datosServidor.execute().get();
+               mostrarMsg(respuesta);
+               jsonObject = new JSONObject(respuesta);
+               jsonArray = jsonObject.getJSONArray("rows");
+               mostrarDatosAmigos();
+           }else{//offline
+               obtenerDatosAmigos();
+           }
+        }catch (Exception e){
+            mostrarMsg("Error: " + e.getMessage());
+        }
     }
     private void obtenerDatosAmigos(){
         try{
@@ -157,7 +178,7 @@ public class lista_amigos extends Activity {
                 alAmigosCopia.clear();
 
                 for (int i=0; i<jsonArray.length(); i++){
-                    jsonObject = jsonArray.getJSONObject(i);
+                    jsonObject = jsonArray.getJSONObject(i).getJSONObject("value");
                     misAmigos = new amigos(
                             jsonObject.getString("idAmigo"),
                             jsonObject.getString("nombre"),
